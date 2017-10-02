1b628dac26d22f057cb1ef171b2267649e180a28
Владимир Кожевников <vladimir.wold@gmail.com>
2017-10-02 21:14:14 +0300

    add amd64

--- docker-entrypoint-init.d/01-install.sh
+++ docker-entrypoint-init.d/01-install.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$ARCH1C" || ( "$ARCH1C" != "i386" && "$ARCH1C" != "amd64" ) ]]; then
-	echo "ERROR: env ARCH1C can only be 'i386' or 'amd64'"
-	exit 1
+if [[ -z "$ARCH1C" || -z "$RAGENT" ]]; then
+    echo "ERROR: empty env ARCH1C or RAGENT"
+    exit 1
 fi
 
-if [[ ! -f /opt/1C/v8.3/$ARCH1C/ragent ]]; then
+if [[ ! -f $RAGENT ]]; then
 
-	DIST_1C=${DIST_1C:="/dist"}
+    DIST_1C=${DIST_1C:="/dist"}
 
-	if [[ -d "$DIST_1C" ]]; then
-		mkdir -p /tmp/dist
-		find $DIST_1C -name \*.tar.gz -exec tar -zxvf {} -C /tmp/dist/ \;
-		find $DIST_1C -name \*.deb -exec cp {} /tmp/dist/ \;
-		DIST_1C=/tmp/dist
-	fi
+    if [[ -d "$DIST_1C" ]]; then
+        mkdir -p /tmp/dist
+        find $DIST_1C -name \*.tar.gz -exec tar -zxvf {} -C /tmp/dist/ \;
+        find $DIST_1C -name \*.deb -exec cp {} /tmp/dist/ \;
+        DIST_1C=/tmp/dist
+    fi
 
-	if [[ -f "$DIST_1C" && ${DIST_1C: -7} == ".tar.gz" ]]; then
-		mkdir -p /tmp/dist
-		tar -zxvf $DIST_1C -C /tmp/dist
-		DIST_1C=/tmp/dist
-	fi
+    if [[ -f "$DIST_1C" && ${DIST_1C: -7} == ".tar.gz" ]]; then
+        mkdir -p /tmp/dist
+        tar -zxvf $DIST_1C -C /tmp/dist
+        DIST_1C=/tmp/dist
+    fi
 
-	install_1c_pkg ()
-	{
-		PKG_NAME=$1
-		REQUIRED=$2
-		PKG_FILE=`find $DIST_1C -type f -regex $DIST_1C/?1c-enterprise[0-9]+-${PKG_NAME}_[0-9]+\.[0-9]+\.[0-9]+-[0-9]+_$ARCH1C\.deb`
-		if [[ -n "$PKG_FILE" ]]; then
-			dpkg -i $PKG_FILE
-		elif [[ "$REQUIRED" == "--required" ]]; then
-			echo "Не найден пакет: $PKG_NAME"
-			return 1
-		fi
-		return 0
-	}
+    install_1c_pkg ()
+    {
+        PKG_NAME=$1
+        REQUIRED=$2
+        PKG_FILE=`find $DIST_1C -type f -regex $DIST_1C/?1c-enterprise[0-9]+-${PKG_NAME}_[0-9]+\.[0-9]+\.[0-9]+-[0-9]+_$ARCH1C\.deb`
+        if [[ -n "$PKG_FILE" ]]; then
+            dpkg -i $PKG_FILE
+        elif [[ "$REQUIRED" == "--required" ]]; then
+            echo "Не найден пакет: $PKG_NAME"
+            return 1
+        fi
+        return 0
+    }
 
-	install_1c_pkg common --required
-	install_1c_pkg server --required
-	install_1c_pkg ws --required
+    install_1c_pkg common --required
+    install_1c_pkg server --required
+    install_1c_pkg ws --required
 
-	rm -rf /tmp/*
+    rm -rf /tmp/*
 
 fi
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,26 +3,36 @@ set -e
 
 if [ "$1" = 'ragent' ]; then
 
-	echo
-	for f in /docker-entrypoint-init.d/*.sh; do
-		echo "$0: running $f"; 
-		/bin/bash "$f";
-	done
+    if [[ -z "$ARCH1C" || ( "$ARCH1C" != "i386" && "$ARCH1C" != "amd64" ) ]]; then
+        echo "ERROR: env ARCH1C can only be 'i386' or 'amd64'"
+        exit 1
+    fi
 
+    echo
+    for f in /docker-entrypoint-init.d/*.sh; do
+        echo "$0: running $f"; 
+        /bin/bash "$f";
+    done
 
-	if [[ -n "$UID_1C" ]]; then
-		usermod -u $UID_1C usr1cv8
-	fi
-	if [[ -n "$GID_1C" ]]; then
-		groupmod -g $GID_1C grp1cv8
-	fi
+    if [[ ! -f $RAGENT ]]; then
+        echo "Не найден $RAGENT"
+        exit 1
+    fi
 
-	mkdir -p /home/usr1cv8/{sync,tmp}
-	chmod 766 /home/usr1cv8
-	chown -R usr1cv8:grp1cv8 /home/usr1cv8
+    if [[ -n "$UID_1C" ]]; then
+        usermod -u $UID_1C usr1cv8
+    fi
 
-	echo "run agent"
-	exec gosu usr1cv8 /opt/1C/v8.3/i386/ragent
+    if [[ -n "$GID_1C" ]]; then
+        groupmod -g $GID_1C grp1cv8
+    fi
+
+    mkdir -p /home/usr1cv8/{sync,tmp}
+    chmod 766 /home/usr1cv8
+    chown -R usr1cv8:grp1cv8 /home/usr1cv8
+
+    echo "run agent"
+    exec gosu usr1cv8 $RAGENT
 
 fi
 
